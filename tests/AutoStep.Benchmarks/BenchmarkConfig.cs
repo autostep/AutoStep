c3428e06b209a80cf262ac45d4f7a70903c61ff6
Alistair Evans <alistairjevans@gmail.com>
2020-01-02 10:05:50 +0000

    Added basic benchmark, that we can use to validate changes to the parser don't scupper performance.

---
+++ tests/AutoStep.Benchmarks/BenchmarkConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+
+namespace AutoStep.Benchmarks
+{
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            Add(DefaultConfig.Instance);
+
+            ArtifactsPath = Path.Combine(AppContext.BaseDirectory, "artifacts", DateTime.Now.ToString("yyyy-mm-dd_hh-MM-ss"));
+
+            Add(MemoryDiagnoser.Default);
+        }
+    }
+}
---
+++ tests/AutoStep.Benchmarks/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Running;
+
+namespace AutoStep.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new BenchmarkSwitcher(AllBenchmarks).Run(args, new BenchmarkConfig());
+        }
+
+        private static readonly Type[] AllBenchmarks =
+        {
+            typeof(FullFileBenchmark)
+        };
+    }
+}
